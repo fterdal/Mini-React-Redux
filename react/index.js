925b42229d78e345e8258b43f5ee3a93c37a391f
Finn Terdal <fterdal@gmail.com>
2018-05-16 16:21:25 -0500

    I've got a simple connected component hooked up

--- react/index.js
+++ react/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { connect, Provider } from 'react-redux'
 
 import store, {
   increment,
   decrement,
 } from '../redux/store'
 
-store.dispatch(increment())
-store.dispatch(decrement())
+const DumbCounter = ({ state }) => (
+  <div>
+    <p>Counter: {state}</p>
+  </div>
+)
+const mapState = (state) => ({ state })
+const SmartCounter = connect(mapState)(DumbCounter)
 
 const App = () => (
   <div>
     <h1>Hello from React!</h1>
+    <SmartCounter />
   </div>
 )
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 )
 
