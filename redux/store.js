796c191ec6ce3ea179227979fb494cfc71bb5114
Finn Terdal <fterdal@gmail.com>
2018-05-16 15:52:53 -0500

    Started to make the store

---
+++ redux/store.js
@@ -0,0 +1,30 @@
+/** Action Types
+ */
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+/** Action Creators
+ */
+export const increment = () => {
+  return { type: INCREMENT_COUNTER }
+}
+export const decrement = () => {
+  return { type: DECREMENT_COUNTER }
+}
+
+/** Reducer
+*/
+const defaultState = 0
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return state + 1
+    case DECREMENT_COUNTER:
+      return state - 1
+    default:
+      return state
+  }
+}
+
+export default reducer
+
